6055fe61e558cefa9ceef066e055e442d90d8054
muhahaha100 <oscarbaso@gmail.com>
2020-08-25 14:57:08 -0600

    website Shows a simple page

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// nothing! :D
+const express = require("express");
+const pug = require("pug");
+const path = require("path");
+const routes = require("./routes/routes");
+
+const app = express();
+
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.use(express.static(path.join(__dirname + "/public")));
+
+app.get("/", routes.index);
+
+app.listen(3000);
