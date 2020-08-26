aa5156b0af6a61c5e4a1a2b835c272471c800ade
muhahaha100 <oscarbaso@gmail.com>
2020-08-25 20:52:44 -0600

    Login and Sessions work
    (forgot to commit this earlier)

--- index.js
+++ index.js
@@ -3,18 +3,41 @@ const pug = require("pug");
 const path = require("path");
 const routes = require("./routes/routes");
 const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const expressSession = require("express-session");
 
 const app = express();
 
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
+
+
+app.use(cookieParser("secret"));
 app.use(express.static(path.join(__dirname + "/public")));
 
 let urlencodedParser = bodyParser.urlencoded({
     extended: true
 });
 
+const checkAuth = (req, res, next) =>{
+    if (req.session.user && req.session.user.isAuthenticated){
+        next();
+    }else{
+        res.redirect("/");
+    }
+};
+
+app.use(expressSession({
+    secret: "super",
+    saveUninitialized: true,
+    resave: true
+}));
+
 app.get("/", routes.index);
+app.get("/private",checkAuth, routes.private);
+app.get("/login", routes.login);
+app.post("/login",urlencodedParser, routes.loginActually);
+app.post("/logout", urlencodedParser, routes.logout);
 app.get("/newUser", routes.newUser);
 app.post("/newUser", urlencodedParser, routes.newUserMade);
 //app.get("/newUser:id", routes.differentRoute);
--- routes/routes.js
+++ routes/routes.js
@@ -1,5 +1,19 @@
 const mongoose = require("mongoose");
+
+//const bcrypt = require("bcryptjs");
+
+//let salt = bcrypt.genSaltSync(10);
+//let hash = bcrypt.hashSync("bacon", salt);
+
+//console.log(salt);
+//console.log(hash);
+
+//console.log(bcrypt.compareSync("bacon", hash));
+//console.log(bcrypt.compareSync("veggies", hash));
+
 const { decodeBase64 } = require("bcryptjs");
+const cookieParser = require("cookie-parser");
+const e = require("express");
 mongoose.Promise = global.Promise;
 
 mongoose.connect("mongodb+srv://user:admin@cluster0.4y4y6.mongodb.net/Cluster0?retryWrites=true&w=majority", {
@@ -66,12 +80,83 @@ exports.form = (req, res) => {
     res.redirect("/");
 };
 
+exports.login = (req, res) => {
+    res.render("login", {
+
+    });
+};
+
+exports.logout = (req, res) => {
+    req.session.destroy(err => {
+        if (err){
+            console.error(err);
+        }else{
+            res.redirect("/");
+        }
+    });
+};
+
+exports.private = (req, res) => {
+    res.render("private", {
+        secretUser: req.session.user.username
+    });
+}
+
+exports.loginActually = (req, res) => {
+    console.log("OMG");
+    User.find((err, user) =>{
+        if (err) return console.error(err);
+
+        let indexFound = -1;
+        console.log("um");
+        for(i = 0; i < user.length; i++){
+            if (user[i]["username"] == req.body.username){
+                if (user[i]["password"] == req.body.password){
+                    indexFound = i;
+                    break;
+                }
+            }
+        }
+
+        if (indexFound > -1){
+            req.session.user = {
+                isAuthenticated: true,
+                username: req.body.username
+            }
+            res.redirect("/private");
+        }else{
+            res.redirect("login");
+            // no user with that username and password
+        }
+    });
+};
+
 
 exports.index = (req,res) => {
     User.find((err, user) => {
         if (err) return console.error(err);
+        let response = "never here before";
+
+        // thanks https://stackoverflow.com/questions/1531093/how-do-i-get-the-current-date-in-javascript
+            // really helped with this part lol
+            let date = new Date();
+            let day = date.getDate().toString();
+            let month = (date.getMonth() + 1).toString();
+            let year = date.getFullYear().toString();
+            let hour = date.getHours() + 1;
+            let minute = date.getMinutes();
+            let seconds = date.getSeconds();
+            let today = month + " / " + day + " / " + year + "  " + hour + ":" + minute + ":" + seconds;
+        
+        if (req.cookies.beenHereBefore != undefined){
+            res.cookie("beenHereBefore", today, {maxAge: 999999999999999});
+            response = req.cookies.beenHereBefore;
+        }else{
+            res.cookie("beenHereBefore", today, {maxAge: 999999999999999});
+        }
         res.render("index", {
-            users: user
+            users: user,
+            lastTime: response
         });
     })
 };
